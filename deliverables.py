8bbea74e14f8cddd6646f94445ab5dd22369ee46
dmadras <dmadras@users.noreply.github.com>
2016-05-04 18:17:30 -0400

    added comments to deliverables.py

--- deliverables.py
+++ deliverables.py
@@ -19,18 +19,23 @@ for b in (0.5, 1, 2):
 
 #random optimization
 res = optimize_ordering_local_search(d, 'diff', tiebreak_flag=True, start_n_random=100)
-ord1 = sorted(res.keys(), key=lambda x: win_pct(res[x][0]))
+#sort the orders by winning percentage, lowest first
+ord1 = sorted(res.keys(), key=lambda x: win_pct(res[x][0])) 
 print('Best order for old hdcp (w/ tiebreak) is: {}, win pct {}'.format(str(ord1[0]), str(win_pct(res[ord1[0]][0]))))
-ord1for50 = sorted(res.keys(), key=lambda x: abs(win_pct(res[x][0]) - 0.5))
+#sort the orders by how close their winning percentages are to 0.5
+ord1for50 = sorted(res.keys(), key=lambda x: abs(win_pct(res[x][0]) - 0.5)) #sort orders by 
 print('Fairest order for old hdcp (w/ tiebreak) is: {}, win pct {}'.format(str(ord1for50[0]), str(win_pct(res[ord1for50[0]][0]))))
 
 
 res = optimize_ordering_local_search(d, 'diff2', tiebreak_flag=True, start_n_random=100)
+#sort the orders by winning percentage, lowest first
 ord2 = sorted(res.keys(), key=lambda x: win_pct(res[x][0]))
 print('Best order new hdcp (w/ tiebreak) is: {}, win pct {}'.format(str(ord2[0]), str(win_pct(res[ord2[0]][0]))))
+#sort the orders by how close their winning percentages are to 0.5
 ord2for50 = sorted(res.keys(), key=lambda x: abs(win_pct(res[x][0]) - 0.5))
 print('Fairest order for new hdcp (w/ tiebreak) is: {}, win pct {}'.format(str(ord2for50[0]), str(win_pct(res[ord2for50[0]][0]))))
 
+#simulate our best and fairest orders
 for (style, order, ord_nm) in [('diff', ord1[0], 'optimized1 with tiebreak'),\
                                ('diff2', ord2[0], 'optimized2 with tiebreak'),\
                                ('diff', ord1for50[0], 'fair-optimized1 with tiebreak'), \
